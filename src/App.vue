fa840dd408c54a5f05417bb05d6a9f876b290baf
Leandro Marques <leandroppmarques@hotmail.com>
2023-02-26 00:54:55 -0300

    estilização da navegação

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,24 @@ export default {
 
 
 <style>
+ body {
+  margin: 0px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: #234;
+ }
+ ul {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+ }
+
+ a {
+  font-weight: bold;
+  text-decoration: none;
+  color: #234;
+ }
+
+ a.router-link-active {
+  color: #4b8;
+ }
 </style>

3d2adf3e0e2db65ba93bda6d4b79aea839d91a43
Leandro Marques <leandroppmarques@hotmail.com>
2023-02-26 23:30:54 -0300

    busca da api home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <TheHeader/>
-    <router-view></router-view>
+    <router-view class="container"></router-view>
   </div>
 </template>
 
@@ -37,5 +37,11 @@ export default {
 
  a.router-link-active {
   color: #4b8;
+ }
+
+ .container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
  }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
     <h1>Home</h1>
+    <p>{{ api }}</p>
   </div>
 </template>
 
 
 <script>
 export default {
-  name: "HomePage"
+  name: "HomePage",
+  data() {
+    return {
+      api: null
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch(`http://localhost:3000/home`)
+        .then(r => r.json())
+        .then(r => {
+          this.api = r;
+        });
+    }
+  },
+  created() {
+    this.fetchData();
+  }
 };
 </script>

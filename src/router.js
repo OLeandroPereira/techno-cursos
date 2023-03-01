85db89d39ab9c4f5e8bbfc845a54af6f979dfa25
Leandro Marques <leandroppmarques@hotmail.com>
2023-02-28 23:49:47 -0300

    criação da view aula

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from './views/Home.vue'
 import Contato from './views/Contato.vue'
 import Cursos from './views/Cursos.vue'
 import Curso from './views/Curso.vue'
+import Aula from './views/Aula.vue'
 
 Vue.use(Router)
 
@@ -32,6 +33,14 @@ export default new Router({
       name: 'curso',
       component: Curso,
       props: true,
+      children: [
+        {
+          path: ":aula",
+          name: "aula",
+          component: Aula,
+          props: true,
+        }
+      ]
     },
   ]
 })
---
+++ src/views/Aula.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ aula }}</p>
+</template>
+
+<script>
+export default {
+  name: "AulaPage",
+  props: ["aula"],
+};
+
+</script>
+
+<style>
+</style>

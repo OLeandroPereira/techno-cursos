6aee7c917d5c52cb02b33f6d0c7c0c2032712420
Leandro Marques <leandroppmarques@hotmail.com>
2023-03-01 16:54:11 -0300

    exclui o setTimeout

--- src/mixins/fetchData.js
+++ src/mixins/fetchData.js
@@ -12,10 +12,6 @@ methods: {
     fetch(`http://localhost:3000${url}`)
       .then(r => r.json())
       .then(r => {
-        setTimeout(() => {
-          this.api = r;
-          this.loading = false;
-        })
       });
   }
 },
